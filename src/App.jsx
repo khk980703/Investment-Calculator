7bcb64fcdd71c0d83d27a03f0d91d89071347e13
Hankyul Kim <hankyulkim@Davids-MacBook-Air-3.local>
2024-12-18 21:19:32 +1300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import Header from "./component/Header";
 import UserInterface from "./component/UserInterface";
+import Results from "./component/Results";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  };
+
   return (
     <>
       <Header />
-      <UserInterface />;
+      <UserInterface userInput={userInput} onChange={handleChange} />
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
     </>
   );
 }
