f3d49be1fb1884397dfa10a2ec28c51d1f567875
Hankyul Kim <hankyulkim@Davids-MacBook-Air-3.local>
2024-12-18 19:44:00 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./component/Header";
+import UserInterface from "./component/UserInterface";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserInterface />;
+    </>
+  );
+}
+
+export default App;
